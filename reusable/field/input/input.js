ccb1f276027675a56a2fd87eff62cdea97a28c60
MicheleDellaquila <70852088+MicheleDellaquila@users.noreply.github.com>
2022-06-04 10:51:48 +0200

    Delete reusable directory

--- reusable/field/input/input.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './input.css';
-
-const Input = React.forwardRef((props, ref) => {
-  return <input ref={ref} className='input' {...props} />;
-});
-
-export default Input;
